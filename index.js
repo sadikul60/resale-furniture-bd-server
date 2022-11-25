c17dbd162814781c849816e0e2b001cb74ac18b1
mdsadikul <mdsadikulislam891@gamil.com>
2022-11-25 10:26:23 +0600

    Added JWT and add verifyJWT

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5000;
 
@@ -15,40 +16,96 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8jrtwg1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// verifyJWT
+function verifyJWT (req, res, next) {
+    console.log('token inside bearer', req.headers.authorization);
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run () {
     try {
         const categoriesCollection = client.db('ResaleFurniture').collection('categories');
         const productsCollection = client.db('ResaleFurniture').collection('products');
         const bookingsCollection = client.db('ResaleFurniture').collection('bookings');
+        const usersCollection = client.db('ResaleFurniture').collection('users');
 
-        // get Categoris
+        // get Categoris from mongodb
         app.get('/categories', async(req, res) => {
             const query = {};
             const categories = await categoriesCollection.find(query).toArray();
             res.send(categories);
         });
 
-        // get products
-        app.get('/products/:id', async(req, res) => {
+        // get products from mongodb (productsCollection from categoris to query specific id)
+        app.get('/categories/:id', async(req, res) => {
             const category = req.params.id;
             const query = {categoryId: category};
             const products = await productsCollection.find(query).toArray();
             res.send(products);
         });
 
-        // post booking data
+        // post from client site and stored mongobd booking data
         app.post('/bookings', async(req, res) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
-        })
+        });
 
-        // post booking data
-        app.get('/bookings', async(req, res) => {
-            const query = {};
+        // get bookings data from mongodb
+        app.get('/bookings', verifyJWT, async(req, res) => {
+            const email = req.query.email;
+
+            const decodedEmail = req.decoded.email;
+
+            if(email !== decodedEmail){
+                return res.status(403).send({message: 'Forbidden access'});
+            }
+
+            const query = {email: email};
             const bookings = await bookingsCollection.find(query).toArray();
-            res.send(bookings);
-        })
+            res.send(bookings.sort().reverse());
+        });
+
+        // post from client site and stored mongodb users data
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        // Get jwt (user)
+        app.get('/jwt', async(req, res) => {
+            const email = req.query.email;
+            const query = { email: email};
+            const user = await usersCollection.findOne(query);
+            
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '7d'});
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({accessToken: ''})
+        });
+
+        // get users data from mongodb
+        app.get('/users', async(req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users.sort().reverse());
+        });
 
     }
     finally {
