b025d8782a0e364371dc3c05c4438a7810f78b17
mdsadikul <mdsadikulislam891@gmail.com>
2022-12-21 19:58:16 +0600

    Edited verifyJWT

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8jrtwg1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// verifyJWT
+verifyJWT
 function verifyJWT (req, res, next) {
     const authHeader = req.headers.authorization;
 
